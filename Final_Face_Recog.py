a52f931cdecb7b406534b36ba4cf1eefb80f86d1
Nakul Makode <134138075+NAKULMAK05@users.noreply.github.com>
2025-01-30 22:50:35 +0530

    Update Final_Face_Recog.py

--- Final_Face_Recog.py
+++ Final_Face_Recog.py
@@ -8,7 +8,7 @@ from PIL import Image
 from torchvision import transforms 
 import sqlite3
 import io
-import time
+import time 
 
 # Set up device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
